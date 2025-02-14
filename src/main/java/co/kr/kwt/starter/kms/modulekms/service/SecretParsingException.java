617569659d46ff22795d7f5787d2c7f45f64de9a
lalalalz <lalalalz@naver.com>
2025-02-14 15:34:43 +0900

    예외 클래스 생성자 설정

--- src/main/java/co/kr/kwt/starter/kms/modulekms/service/SecretParsingException.java
+++ src/main/java/co/kr/kwt/starter/kms/modulekms/service/SecretParsingException.java
@@ -3,6 +3,22 @@ package co.kr.kwt.starter.kms.modulekms.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 public class SecretParsingException extends RuntimeException {
+
     public SecretParsingException(String failedToParseSecretResponse, JsonProcessingException e) {
+    }
+
+    public SecretParsingException() {
+    }
+
+    public SecretParsingException(String message) {
+        super(message);
+    }
+
+    public SecretParsingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public SecretParsingException(Throwable cause) {
+        super(cause);
     }
 }
